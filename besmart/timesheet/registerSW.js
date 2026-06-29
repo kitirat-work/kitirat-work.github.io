1135fd4f6cec00a999e848a76f6b7e87d65c4377
Kittipan (Tee) <kitirat.work@gmail.com>
2026-06-29 17:38:05 +0700

    deploy: timesheet

--- besmart/timesheet/registerSW.js
+++ besmart/timesheet/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/besmart/timesheet/sw.js', { scope: '/besmart/timesheet/' })})}
